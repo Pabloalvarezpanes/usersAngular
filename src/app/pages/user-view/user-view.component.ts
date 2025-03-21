5e3cf797d7529bedbb455754fef7ee6db1c7c184
Pablo Álvarez <pablo.alvarezpanes949@comunidadunir.net>
2025-03-21 22:45:15 +0100

    maquetación y configuración de la vista del detalle de usuario

--- src/app/pages/user-view/user-view.component.ts
+++ src/app/pages/user-view/user-view.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
+import { IUser } from '../../interfaces/iuser.interface';
+import { UsersService } from '../../services/users.service';
+import { ButtonsComponent } from '../../shared/buttons/buttons.component';
 
 @Component({
   selector: 'app-user-view',
-  imports: [],
+  imports: [ButtonsComponent],
   templateUrl: './user-view.component.html',
   styleUrl: './user-view.component.css'
 })
 export class UserViewComponent {
+
+  @Input() idUser: string = "";
+  myUser: IUser | any;
+  userServices = inject(UsersService);
+
+  async ngOnInit() {
+    try {
+      this.myUser = await this.userServices.getById(this.idUser);
+    } catch (error){
+      console.log(error);
+    }
+  }
 
 }
