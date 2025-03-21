d28b4c16c6a80199abf4c29a3a6f563ce3fd4731
Pablo Álvarez <pablo.alvarezpanes949@comunidadunir.net>
2025-03-21 21:42:49 +0100

    creacion de nuevo componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,18 +12,5 @@ import { IResponse } from './interfaces/iresponse.interface';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  arrUsers: IUser[] = [];
-  userServices = inject(UsersService);
-  @Input() unUser!: IUser;
-  linkNext!: number;
-  linkPrev!: number;
-
-  async chargeUsers() {
-    try {
-      let response: IResponse = await this.userServices.getAll();
-      this.linkNext = response.page +1;
-      this.linkPrev = response.page;
-      this.arrUsers = response.results;
-    } catch (error){}
-  }
+  title = 'user';
 }
