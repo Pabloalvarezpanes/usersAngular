b233a74a4050501568dbfdd64a4baf7a3d57e345
Pablo Álvarez <pablo.alvarezpanes949@comunidadunir.net>
2025-03-21 22:07:22 +0100

    configuracion del componente buttons junto a sweetAlert2Module

--- src/app/shared/buttons/buttons.component.ts
+++ src/app/shared/buttons/buttons.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
+import { IUser } from '../../interfaces/iuser.interface';
+import { UsersService } from '../../services/users.service';
+import { Router, RouterLink } from '@angular/router';
+import 'sweetalert2/src/sweetalert2.scss';
+import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
+
+
+
 
 @Component({
   selector: 'app-buttons',
-  imports: [],
+  imports: [RouterLink, SweetAlert2Module],
   templateUrl: './buttons.component.html',
   styleUrl: './buttons.component.css'
 })
 export class ButtonsComponent {
+  @Input() myUser!:IUser;
+  userServices = inject(UsersService);
+  titularAlerta:string = "";
+  router = inject(Router);
+
 
 }
